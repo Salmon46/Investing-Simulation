5f4ac81ce14ee180e4c624a133bccc386c62f23a
Salmon46 <85119060+Salmon46@users.noreply.github.com>
2024-03-23 13:23:21 -0500

    Altered Portfolio database and improved functionality of buy function

--- functionality-config.py
+++ functionality-config.py
@@ -3,7 +3,7 @@ import pandas as pd
 import psycopg2 as dbEditor
 from datetime import datetime, timedelta
 
-investment_id = 0
+initial_investment_id = 1
 
 connection_details = dbEditor.connect(
     host="ceu9lmqblp8t3q.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com",
@@ -26,12 +26,11 @@ def ticker_lookup():
 
 
 def buy():
-    global investment_id
-    investment_id = investment_id + 1
+    global initial_investment_id
     
     ticker = input("Enter the ticker of the stock you would like to buy\n")
 
-    amount = input("How many shares would you like to buy?\n")
+    shares_bought = input("How many shares would you like to buy?\n")
 
     end_date = datetime.today()
     start_date = end_date - timedelta(days=1)
@@ -39,11 +38,23 @@ def buy():
     close_price_df = pd.DataFrame(data['Close'])
     close_price = float(close_price_df.iat[0, 0])
 
-    money_spent = float(amount)*float(close_price)
+    money_spent = float(shares_bought)*float(close_price)
 
-    table_modification = f'INSERT INTO "Portfolio" (investment_id, ticker, shares_bought, price_of_share_at_buy, money_spent)' + f" VALUES ( {int(investment_id)},'{str(ticker)}', {float(amount)}, {float(close_price)}, {float(money_spent)})"
+    sql = f'SELECT * FROM "Portfolio"' + f" WHERE ticker='{str(ticker)}'"
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    if(result):
+        table_modification = f'UPDATE "Portfolio"' + f" SET shares_bought={float(result[3])+float(shares_bought)}, money_spent={float(result[2])+float(money_spent)}" + f" WHERE ticker='{ticker}'"
+    else:
+        sql = f'SELECT MAX(investment_id) FROM "Portfolio"'
+        cursor.execute(sql)
+        result2 = cursor.fetchone()
+        if(result2[0] != None):
+            table_modification = f'INSERT INTO "Portfolio" (investment_id, ticker, money_spent, shares_bought)' + f" VALUES ( {int(initial_investment_id)+int(result2[0])},'{str(ticker)}', {float(money_spent)}, {float(shares_bought)})"
+        else:
+            table_modification = f'INSERT INTO "Portfolio" (investment_id, ticker, money_spent, shares_bought)' + f" VALUES ( {int(initial_investment_id)},'{str(ticker)}', {float(money_spent)}, {float(shares_bought)})"
+
     cursor.execute(table_modification)
     connection_details.commit()
     cursor.close()
     connection_details.close()
-
