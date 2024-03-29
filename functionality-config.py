21bae22c3765e33edbc2fee9b3c4da4eea8f5b27
Salmon46 <85119060+Salmon46@users.noreply.github.com>
2024-03-29 13:52:23 -0500

    Added Function to Continuously Update Portfolio

--- functionality-config.py
+++ functionality-config.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import pandas as pd
 import psycopg2 as dbEditor
 from datetime import datetime, timedelta
+import time
 
 initial_investment_id = 1
 
@@ -24,6 +25,29 @@ def ticker_lookup():
 
     print(close_prices_df)
 
+def continuous_portfolio_update():
+    while True:
+        sql = 'SELECT ticker FROM "Portfolio"'
+        cursor.execute(sql)
+        ticker_tuple = cursor.fetchall()
+
+        sql2 = 'SELECT shares_owned FROM "Portfolio"'
+        cursor.execute(sql2)
+        shares_owned_tuple = cursor.fetchall()
+
+        for x in ticker_tuple:
+            end_date = datetime.today()
+            start_date = end_date - timedelta(days=1)
+            data = yf.download(x[0],start_date, end_date)
+            close_price_df = pd.DataFrame(data['Close'])
+            close_price = float(close_price_df.iat[0, 0])
+
+            index = int(ticker_tuple.index(x))
+
+            sql3 = f'UPDATE "Portfolio"' + f" SET investment_value='{int(float(close_price)*float(shares_owned_tuple[index][0]))}'" + f"WHERE ticker='{x[0]}'"
+            cursor.execute(sql3)
+
+        time.sleep(5)
 
 def buy():
     global initial_investment_id
@@ -44,15 +68,15 @@ def buy():
     cursor.execute(sql)
     result = cursor.fetchone()
     if(result):
-        table_modification = f'UPDATE "Portfolio"' + f" SET shares_bought={float(result[3])+float(shares_bought)}, money_spent={float(result[2])+float(money_spent)}" + f" WHERE ticker='{ticker}'"
+        table_modification = f'UPDATE "Portfolio"' + f" SET shares_owned={float(result[3])+float(shares_bought)}, investment_value={float(result[2])+float(money_spent)}" + f" WHERE ticker='{ticker}'"
     else:
         sql = f'SELECT MAX(investment_id) FROM "Portfolio"'
         cursor.execute(sql)
         result2 = cursor.fetchone()
         if(result2[0] != None):
-            table_modification = f'INSERT INTO "Portfolio" (investment_id, ticker, money_spent, shares_bought)' + f" VALUES ( {int(initial_investment_id)+int(result2[0])},'{str(ticker)}', {float(money_spent)}, {float(shares_bought)})"
+            table_modification = f'INSERT INTO "Portfolio" (investment_id, ticker, investment_value, shares_owned)' + f" VALUES ( {int(initial_investment_id)+int(result2[0])},'{str(ticker)}', {float(money_spent)}, {float(shares_bought)})"
         else:
-            table_modification = f'INSERT INTO "Portfolio" (investment_id, ticker, money_spent, shares_bought)' + f" VALUES ( {int(initial_investment_id)},'{str(ticker)}', {float(money_spent)}, {float(shares_bought)})"
+            table_modification = f'INSERT INTO "Portfolio" (investment_id, ticker, investment_value, shares_owned)' + f" VALUES ( {int(initial_investment_id)},'{str(ticker)}', {float(money_spent)}, {float(shares_bought)})"
 
     cursor.execute(table_modification)
     connection_details.commit()
