0645edb7e137a36854d90a9261b8e5d9fb621ceb
Salmon46 <85119060+Salmon46@users.noreply.github.com>
2024-03-31 13:27:44 -0500

    testing app connection to interface

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template
 
 investing_simulation = Flask(__name__)
 
-investing_simulation.config['SECRET_KEY'] = 'Sudin@110508'
-
 @investing_simulation.route("/")
-def webpage():
-    
-    return render_template('Interface.html')
+def load_webpage():
+    return render_template('./Interface.html')
