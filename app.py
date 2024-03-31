e13a709154c5982fe515e3e2feb50b06c712cbfc
Salmon46 <85119060+Salmon46@users.noreply.github.com>
2024-03-31 13:32:51 -0500

    testing app connection to interface

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ investing_simulation = Flask(__name__)
 
 @investing_simulation.route("/")
 def load_webpage():
-    return render_template('./Interface.html')
+    return render_template('Interface.html')
